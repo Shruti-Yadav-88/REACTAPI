5d485c382499b765a5cbb045ad4a6c29d29d7498
Shruti-Yadav-88 <shrutiyadav9849@gmail.com>
2025-07-03 08:38:04 +0545

    Update API

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,56 @@
+// GbeEn7
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 
-const App=() =>{
+const App = () => {
   const BaseUrl = "https://jsonplaceholder.typicode.com";
   const UserUrl = `${BaseUrl}/users`;
 
-const [users,setUsers] = useState([]);
-useEffect(()=>{
-const fetchUsers=async()=>{
-  try {
-  const responsegi = await axios.get(UserUrl);
-  setUsers(response.data);
-  } catch (e){
-    console.error("Error fetching users:", e)
-  }
+  const [users, setUsers] = useState([]);
+  console.log(users);
+  // useEffect(()=>{},[])
+  const deleteUser = async(id) => {
+    try{
+   await axios.delete(`${UserUrl}/${id}`);
+    setUsers(users.filter((user)=>user.id !== id )) 
+    } catch (e){
+      console.error("Error deleting user", e)
+    }
+  };
 
-} 
-fetchUsers();
-  }, [])
-  return( 
-  <>This is app component
-  {users.map((user) => (
-    <div key={user.id}>{user.name}</div>
-   ))}
-    </>)
-  
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get(UserUrl);
+        setUsers(response.data);
+      } catch (e) {
+        console.error("Error fetching users:", e);
+      }
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <>
+      This is app component
+      {users.map((user) => (
+        <Fragment key={user.id}>
+          <div
+            style={{
+              backgroundColor: "lightblue",
+              margin: "10px",
+              padding: "10px",
+              borderRadius: "5px",
+            }}
+          >
+            {user.name}
+          </div>
+          <button style={{ backgroundColor: "red" }}
+          onClick={()=> deleteUser(user.id)}>Delete {user.name}</button>
+        </Fragment>
+      ))}
+    </>
+  );
 };
 
-export default App
+export default App;
